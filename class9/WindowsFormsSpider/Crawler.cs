11460e808d2322a4413fa0eac48d9862e1f1d90a
Sting-Scorpion <wbc2001@126.com>
2021-06-02 10:21:16 +0800

    class9

---
+++ class9/WindowsFormsSpider/Crawler.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Net;
+using System.Threading;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace WindowsFormsSpider
+{
+    class Crawler
+    {
+        //private Hashtable urls = new Hashtable();
+        //private int count = 0;
+        public event Action<Crawler> CrawlerStopped;//爬完的事件
+        public event Action<Crawler, string, string> PageDownloaded;
+        //URL解析表达式
+        public static readonly string urlParseRegex = @"^(?<site>(?<protocal>https?)://(?<host>[\w.-]+)(:\d+)?($|/))(\w+/)*(?<file>[^#?]*)";
+        //URL检测表达式，用于在HTML文本中查找URL
+        public static readonly string strRef = @"(href|HREF)[]*=[]*[""'](?<url>[^""'#>]+)[""']";
+        //主机过滤规则
+        public string HostFilter { get; set; }
+        //待下载队列
+        Queue<string> pending = new Queue<string>();
+        //已下载网页
+        public Dictionary<string, bool> DownloadedPages { get; } = new Dictionary<string, bool>();
+        //文件过滤规则
+        public string FileFilter { get; set; }
+        //最大下载数量
+        public int MaxPage { get; set; }
+        //起始网址
+        public string StartURL { get; set; }
+        //网页编码
+        public Encoding HtmlEncoding { get; set; }
+
+        public Crawler()
+        {
+            MaxPage = 40;
+            HtmlEncoding = Encoding.UTF8;
+        }
+
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string fileName = DownloadedPages.Count.ToString();
+                File.WriteAllText("cnblogs" + fileName, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+
+        public void Parse(string html, string current)
+        {
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                string linkUrl = match.Groups["url"].Value;
+                if (linkUrl == null || linkUrl == "" || linkUrl.StartsWith("javascript:")) continue;
+
+                linkUrl = TransToComplete(linkUrl, current);//转绝对路径
+                                                            //解析出host和file两个部分，进行过滤
+                Match linkUrlMatch = Regex.Match(linkUrl, urlParseRegex);
+                string host = linkUrlMatch.Groups["host"].Value;
+                string file = linkUrlMatch.Groups["file"].Value;
+                if (Regex.IsMatch(host, HostFilter) && Regex.IsMatch(file, FileFilter)
+                  && !DownloadedPages.ContainsKey(linkUrl))
+                {
+                    pending.Enqueue(linkUrl);
+                }
+                //strRef = match.Value.Substring(match.Value.IndexOf('=') + 1).Trim
+                //    ('"', '\"', '#', '>');
+                //if (strRef.Length == 0) continue;
+                //if (urls[strRef] == null) urls[strRef] = false;
+            }
+        }
+        public void Start()
+        {
+            DownloadedPages.Clear();
+            pending.Clear();
+            pending.Enqueue(StartURL);
+
+            while (DownloadedPages.Count < MaxPage && pending.Count > 0)
+            {
+                string url = pending.Dequeue();
+                try
+                {
+                    string html = DownLoad(url); // 下载
+                    DownloadedPages[url] = true;
+                    PageDownloaded(this, url, "success");
+                    Parse(html, url);//解析,并加入新的链接
+                }
+                catch (Exception ex)
+                {
+                    PageDownloaded(this, url, "  Error:" + ex.Message);
+                }
+            }
+            CrawlerStopped(this);
+        }
+        //private void Crawl(string startURL)
+        //{
+        //    urls.Add(startURL, false);
+        //    MessageBox.Show("开始爬行......");
+        //    while (true)
+        //    {
+        //        string current = null;
+        //        foreach (string url in urls.Keys)
+        //        {
+        //            if ((bool)urls[url]) continue;
+        //            current = url;
+        //        }
+        //        if (current == null || count > 10) break;
+        //        MessageBox.Show("爬行" + current + "页面！");
+        //        string html = DownLoad(current);
+        //        urls[current] = true;
+        //        count++;
+        //        Parse(html, url);
+        //    }
+        //    MessageBox.Show("爬行结束");
+        //}
+        //转换为完整路径
+        private string TransToComplete(string url, string current)
+        {
+            if (current.EndsWith("/")) current = current.Substring(0, current.Length - 1);
+            if (url.Contains("://")) return url;    //完整路径
+            if (url.StartsWith("//"))
+            {
+                Match urlMatch = Regex.Match(current, urlParseRegex);
+                string protocal = urlMatch.Groups["protocal"].Value;
+                return protocal + ":" + url;
+            }
+            if (url.StartsWith("/"))
+            {
+                Match urlMatch = Regex.Match(current, urlParseRegex);
+                String site = urlMatch.Groups["site"].Value;
+                return site.EndsWith("/") ? site + url.Substring(1) : site + url;
+            }
+
+            if (url.StartsWith("../"))
+            {
+                url = url.Substring(3);
+                int idx = current.LastIndexOf('/');
+                return TransToComplete(url, current.Substring(0, idx));
+            }
+
+            if (url.StartsWith("./"))
+            {
+                return TransToComplete(url.Substring(2), current);
+            }
+            //非上述开头的相对路径
+            int end = current.LastIndexOf("/");
+            return current.Substring(0, end) + "/" + url;
+        }
+    }
+}
---
+++ class9/WindowsFormsSpider/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsSpider
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Index = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.URL = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.resultBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.URLtxt = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultBindingSource)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 116);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1051, 339);
+            this.panel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Index,
+            this.URL,
+            this.Status});
+            this.dataGridView1.DataSource = this.resultBindingSource;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(1051, 339);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Index
+            // 
+            this.Index.DataPropertyName = "Index";
+            this.Index.HeaderText = "序号";
+            this.Index.MinimumWidth = 6;
+            this.Index.Name = "Index";
+            this.Index.Width = 125;
+            // 
+            // URL
+            // 
+            this.URL.DataPropertyName = "URL";
+            this.URL.HeaderText = "URL";
+            this.URL.MinimumWidth = 6;
+            this.URL.Name = "URL";
+            this.URL.Width = 500;
+            // 
+            // Status
+            // 
+            this.Status.DataPropertyName = "Status";
+            this.Status.HeaderText = "状态";
+            this.Status.MinimumWidth = 6;
+            this.Status.Name = "Status";
+            this.Status.Width = 125;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.lblInfo);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.StartBtn);
+            this.panel2.Controls.Add(this.URLtxt);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1051, 116);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 2;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(678, 45);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(78, 26);
+            this.StartBtn.TabIndex = 1;
+            this.StartBtn.Text = "爬取";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // URLtxt
+            // 
+            this.URLtxt.Location = new System.Drawing.Point(117, 46);
+            this.URLtxt.Name = "URLtxt";
+            this.URLtxt.Size = new System.Drawing.Size(535, 25);
+            this.URLtxt.TabIndex = 0;
+            this.URLtxt.Text = "http://www.cnblogs.com/dstang2000/";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(53, 91);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(63, 15);
+            this.lblInfo.TabIndex = 3;
+            this.lblInfo.Text = "waiting";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1051, 455);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resultBindingSource)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.TextBox URLtxt;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Index;
+        private System.Windows.Forms.DataGridViewTextBoxColumn URL;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Status;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.BindingSource resultBindingSource;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
